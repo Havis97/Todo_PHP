71564e87d806eefa79cf77318aa35ebe04aec3eb
Havis <alhavis1995@rocketmail.com>
2026-06-01 11:53:53 +0700

    update todos dan file todos

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
-// Your PHP code here
+ $todos = [ ];
+
+    if(file_exists('todos.txt')) {
+        $file = file_get_contents('todos.txt');
+        $todos = unserialize($file);
+    }
+
+    // cek apakah ada data yang dikirimkan melalui form
+    if(isset($_POST['todo'])) {
+        $data = $_POST['todo'];
+        // tambahkan data ke array todos
+        $todos[] = [
+            'todo' =>$data,
+            'status' => 0,
+            ];
+            $daftar_todo = serialize($todos);
+            file_put_contents('todos.txt', $daftar_todo);
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,28 +28,20 @@
 <body>
     <h1>My Todo App</h1>
     <!-- Form Input -->
-    <form action="">
+    <form method="POST">
         <label>My To Do List</label>
-        <input type="text" name="todo" placeholder="Enter your task here">
+        <input type="text" name="todo">
         <button type="submit">Simpan</button>
     </form>
     <!-- list todo -->
     <ul>
+        <?php foreach($todos as $key => $value):?>
         <li>
             <input type="checkbox" name="todo">
-            <label>Learn PHP</label>
+            <label><?php echo $value['todo']; ?></label>
             <a href="#">Hapus</a>
         </li>
-        <li>
-            <input type="checkbox" name="todo">
-            <label>Learn JavaScript</label>
-            <a href="#">Hapus</a>
-        </li>
-        <li>
-            <input type="checkbox" name="todo">
-            <label>Learn Laravel</label>
-            <a href="#">Hapus</a>
-        </li>
+        <?php endforeach; ?>
     </ul>
-</bod`y>`
+</body>
 </html>
