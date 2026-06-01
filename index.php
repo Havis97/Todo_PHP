e31f40f11d75981012bb27049f7c9e904eab6df0
Havis <alhavis1995@rocketmail.com>
2026-06-01 11:30:00 +0700

    first

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+// Your PHP code here
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Todo App</title>
+</head>
+<body>
+    <h1>My Todo App</h1>
+    <!-- Form Input -->
+    <form action="">
+        <label>My To Do List</label>
+        <input type="text" name="todo" placeholder="Enter your task here">
+        <button type="submit">Simpan</button>
+    </form>
+    <!-- list todo -->
+    <ul>
+        <li>
+            <input type="checkbox" name="todo">
+            <label>Learn PHP</label>
+            <a href="#">Hapus</a>
+        </li>
+        <li>
+            <input type="checkbox" name="todo">
+            <label>Learn JavaScript</label>
+            <a href="#">Hapus</a>
+        </li>
+        <li>
+            <input type="checkbox" name="todo">
+            <label>Learn Laravel</label>
+            <a href="#">Hapus</a>
+        </li>
+    </ul>
+</bod`y>`
+</html>
